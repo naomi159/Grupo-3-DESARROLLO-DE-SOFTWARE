926fed9f9d05dd7877376725d305dc67aa90de15
Naomi Isabel Masias Uscamayta <72511623+naomi159@users.noreply.github.com>
2021-08-16 16:02:40 -0500

    Update ConexionSQL.cs

--- SistemaTutoria/Datos/ConexionSQL.cs
+++ SistemaTutoria/Datos/ConexionSQL.cs
@@ -10,7 +10,8 @@ namespace Datos
 {
     public class ConexionSQL
     {
-        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-H4RJ2LR;Initial Catalog=Tutoria;Integrated Security=True");
+        //SqlConnection conn = new SqlConnection("Data Source=DESKTOP-H4RJ2LR;Initial Catalog=Tutoria;Integrated Security=True");
+        SqlConnection conn = new SqlConnection("Data Source=proyectogrupo3-server.database.windows.net;Initial Catalog=Tutoria;User ID= Administrador; Password=Teamoyannick1");
         public DataTable Select(string consulta)
         {
             DataTable dt = new DataTable();
